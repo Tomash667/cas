d3cd1ab08cbd4c067bb7e07d3769ab8096fbc448
Tomashu <tomash4my@gmail.com>
2016-07-15 20:51:48 +0200

    added missing unit test, readme

---
+++ readme.md
@@ -0,0 +1,2 @@
+##cas: Ca Scripting language [![Build status](https://ci.appveyor.com/api/projects/status/vec4jvercblgrsxn?svg=true)](https://ci.appveyor.com/project/Tomash667/cas)
+Scripting language based on C++.
--- tests/unittest1.cpp
+++ tests/unittest1.cpp
@@ -1,11 +1,6 @@
 #include "stdafx.h"
 #include "CppUnitTest.h"
 
-#if ((_CI_MODE - 1) == 0)
-#	define CI_MODE
-#endif
-
-
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 using namespace std;
 
@@ -22,11 +17,17 @@ struct PackedData
 	bool optimize;
 };
 
-#ifdef CI_MODE
-const int DEFAULT_TIMEOUT = 60;
-#else
-const int DEFAULT_TIMEOUT = 1;
-#endif
+const bool CI_MODE = ((_CI_MODE - 1) == 0);
+const int DEFAULT_TIMEOUT = (CI_MODE ? 60 : 1);
+
+#define TestMethod(name) \
+	TEST_METHOD(name) \
+	{ \
+		if(!CI_MODE) \
+			Logger::WriteMessage("\n***** Test case: " #name " ******************************\n"); \
+		name##_impl(); \
+	} \
+	void name##_impl()
 
 unsigned __stdcall ThreadStart(void* data)
 {
@@ -94,14 +95,12 @@ namespace tests
 		{
 			event_output.clear();
 
-#ifndef CI_MODE
-			if(input[0] != 0)
+			if(!CI_MODE && input[0] != 0)
 			{
-				Logger::WriteMessage("Script input:\n");
+				Logger::WriteMessage("\nScript input:\n");
 				Logger::WriteMessage(input);
-				Logger::WriteMessage("\n\n");
+				Logger::WriteMessage("\n");
 			}
-#endif
 
 			string path(Format("../../../cases/%s", filename));
 			std::ifstream ifs(path);
@@ -127,148 +126,135 @@ namespace tests
 				cstring output = Format("Script parsing failed. Parse output:\n%s\nOutput: %s", event_output.c_str(), ss);
 				Assert::Fail(GetWC(output).c_str());
 			}
-#ifndef CI_MODE
-			Logger::WriteMessage("Script output:\n");
-			Logger::WriteMessage(ss);
-#endif
+
+			if(!CI_MODE)
+			{
+				Logger::WriteMessage("\nScript output:\n");
+				Logger::WriteMessage(ss);
+				Logger::WriteMessage("\n");
+			}
+
 			Assert::AreEqual(output, ss, "Invalid output.");
 		}
 		
-		TEST_METHOD(Simple)
+		TestMethod(Simple)
 		{
-			Logger::WriteMessage("Test case: Simple ******************************\n");
 			Test("simple.txt", "4", "Podaj a: Odwrocone a: -4");
 		}
 
-		TEST_METHOD(Math)
+		TestMethod(Math)
 		{
-			Logger::WriteMessage("Test case: Math ******************************\n");
 			Test("math.txt", "1 2 3 4", "7\n2\n3\n-2\n0\n37\n1\n");
 			Test("math.txt", "8 15 4 3", "68\n119\n23\n-120\n0\n37\n0\n");
 		}
 
-		TEST_METHOD(Assign)
+		TestMethod(Assign)
 		{
-			Logger::WriteMessage("Test case: Assign ******************************\n");
 			Test("assign.txt", "", "5\n1\n8\n4\n2\n7\ntrue\n");
 		}
 
-		TEST_METHOD(String)
+		TestMethod(String)
 		{
-			Logger::WriteMessage("Test case: String ******************************\n");
 			Test("string.txt", "Tomash 1990", "Podaj imie: Podaj rok urodzenia: Witaj Tomash! Masz 26 lat.");
 		}
 
-		TEST_METHOD(Float)
+		TestMethod(Float)
 		{
-			Logger::WriteMessage("Test case: Float ******************************\n");
 			Test("float.txt", "7", "153.934\n5.5\n5.5\n3\n");
 		}
 
-		TEST_METHOD(Parentheses)
+		TestMethod(Parentheses)
 		{
-			Logger::WriteMessage("Test case: Parentheses ******************************\n");
 			Test("parentheses.txt", "", "-5\n20\n0\n24\n");
 		}
 
-		TEST_METHOD(Bool)
+		TestMethod(Bool)
 		{
-			Logger::WriteMessage("Test case: Bool ******************************\n");
 			Test("bool.txt", "7 8", "false\ntrue\nfalse\nfalse\ntrue\ntrue\n-1\n8\ntrue\nfalse\n");
 		}
 
-		TEST_METHOD(CompOperators)
+		TestMethod(CompOperators)
 		{
-			Logger::WriteMessage("Test case: CompOperators ******************************\n");
 			Test("comp_operators.txt", "", "true\ntrue\ntrue\ntrue\ntrue\nfalse\nfalse\ntrue\nfalse\n");
 		}
 
-		TEST_METHOD(IfElse)
+		TestMethod(IfElse)
 		{
-			Logger::WriteMessage("Test case: IfElse ******************************\n");
 			Test("if_else.txt", "1 1", "a == b\n0\n1\n4\n6\n7\n");
 			Test("if_else.txt", "2 1", "a > b\n2\n4\n6\n7\n");
 			Test("if_else.txt", "1 2", "a < b\n2\n4\n6\n7\n", false);
 		}
 
-		TEST_METHOD(While)
+		TestMethod(While)
 		{
-			Logger::WriteMessage("Test case: While ******************************\n");
 			Test("while.txt", "3", "***yyyy/++--");
 			Test("while.txt", "4", "****yyyy/++--", false);
 		}
 
-		TEST_METHOD(TypeFunc)
+		TestMethod(TypeFunc)
 		{
-			Logger::WriteMessage("Test case: TypeFunc ******************************\n");
 			Test("type_func.txt", "-9", "4\n9\n6\n3.1415\n");
 		}
 
-		TEST_METHOD(Block)
+		TestMethod(Block)
 		{
-			Logger::WriteMessage("Test case: Block ******************************\n");
 			Test("block.txt", "", "4\n0\n8\n");
 		}
 
-		TEST_METHOD(For)
+		TestMethod(For)
 		{
-			Logger::WriteMessage("Test case: For ******************************\n");
 			Test("for.txt", "", "0123456789***+++++-----");
 			Test("for.txt", "", "0123456789***+++++-----", false);
 		}
 
-		TEST_METHOD(IncDec)
+		TestMethod(IncDec)
 		{
-			Logger::WriteMessage("Test case: IncDec ******************************\n");
 			Test("inc_dec.txt", "7", "8\n10\n10\n11\n");
 		}
 
-		TEST_METHOD(UserFunc)
+		TestMethod(UserFunc)
 		{
-			Logger::WriteMessage("Test case: UserFunc ******************************\n");
 			Test("user_func.txt", "1 2 3 4 5 6 7 8", "3\n6\n9\n15\n15\n");
 		}
 
-		TEST_METHOD(FuncDefParams)
+		TestMethod(FuncDefParams)
 		{
-			Logger::WriteMessage("Test case: FuncDefParams ******************************\n");
 			Test("func_def_params.txt", "", "a: 3, b: 4\na: 7, b: 4\na: 11, b: 13\n");
 		}
 
-		TEST_METHOD(DefValue)
+		TestMethod(DefValue)
 		{
-			Logger::WriteMessage("Test case: DefValue ******************************\n");
 			Test("def_value.txt", "", "false\n0\n0\n[]\nfalse\n0\n0\n[]\n");
 		}
 
-		TEST_METHOD(BitOp)
+		TestMethod(BitOp)
 		{
-			Logger::WriteMessage("Test case: BitOp ******************************\n");
 			Test("bit_op.txt", "", "17029\n65534\n38208\n792166400\n7776\n33280\n41701\n28212\n-1194328064\n-9330688\n");
 		}
 
-		TEST_METHOD(Class)
+		TestMethod(Class)
 		{
-			Logger::WriteMessage("Test case: Class ******************************\n");
 			Test("class.txt", "", "x:3 y:4\nx:7 y:11\nx:5 y:8\n7\n6\n55\nx:5 y:4\n");
 		}
 
-		TEST_METHOD(ComplexClassResult)
+		TestMethod(ComplexClassResult)
 		{
-			Logger::WriteMessage("Test case: ComplexClassResult ******************************\n");
 			Test("complex_class_result.txt", "", "x:1.6 y:2.3 z:4.1\nx:1.6 y:2.4 z:4.1\nx:0 y:0\nx:13 y:13\nx:7 y:3\nx:3.14 y:0.0015\n");
 		}
 
-		TEST_METHOD(FuncOverload)
+		TestMethod(FuncOverload)
 		{
-			Logger::WriteMessage("Test case: FuncOverload ******************************\n");
 			Test("func_overload.txt", "", "4\n\nvoid\nint 1\nstring test\nfloat 3.14, int 3\n");
 		}
 
-		TEST_METHOD(ScriptClass)
+		TestMethod(ScriptClass)
 		{
-			Logger::WriteMessage("Test case: ScriptClass ******************************\n");
 			Test("script_class.txt", "", "x:3\ny:0.14\nc:true\nd:false\ng:10\nh:false\n");
+		}
+
+		TestMethod(ScriptClassFunc)
+		{
+			Test("script_class_func.txt", "", "3.5\n3.5\n19.5\n20.5\n");
 		}
 	};
 }
