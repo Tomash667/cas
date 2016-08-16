d8a6da0cf63c77d1595a38273b43e1d2de7f389a
Tomashu <tomash4my@gmail.com>
2016-08-16 15:11:56 +0200

    fixed release build assert_handler

--- Base.cpp
+++ Base.cpp
@@ -16,6 +16,7 @@ AssertHandler _assert_handler;
 
 void assert_handler(cstring msg, cstring file, uint line)
 {
+#ifdef _DEBUG
 	if(_assert_handler)
 		_assert_handler(msg, file, line);
 	else
@@ -26,6 +27,7 @@ void assert_handler(cstring msg, cstring file, uint line)
 		mbstowcs(file_buf, file, 256);
 		_wassert(msg_buf, file_buf, line);
 	}
+#endif
 }
 
 void set_assert_handler(AssertHandler handler)
